45cb53a7f55cd049b0598d9dfaf1708ed2f83793
AndreyCNF <resende.andrey@yahoo.com.br>
2020-05-07 12:23:37 -0300

    frontend da aplicação

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './content/home/home.component';
+import { ReceitasDetalheComponent } from './content/receitas-detalhe/receitas-detalhe.component';
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: "AngularFood"
+    }
+  },
+    {
+      path: 'receitas/:id',
+      component: ReceitasDetalheComponent,
+    },
+    {
+      path: '',
+      redirectTo: 'home',
+      pathMatch: 'full'
+    }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './content/home/home.component';
+import { ReceitasDetalheComponent } from './content/receitas-detalhe/receitas-detalhe.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ReceitasDetalheComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
